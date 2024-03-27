357030da4210fc72d08cd7965bbf940c62958c8d
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-03-27 09:30:02 -0700

    fully defined member model

--- models/Member.ts
+++ models/Member.ts
@@ -1,24 +1,49 @@
 import { DataTypes } from 'sequelize'
 import { sequelize } from '../db/db.connection'
 
-const Member = sequelize.define('Member', {
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
+const Member = sequelize.define(
+  'Member', 
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    profile_pic: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    quizzes: {
+      type: DataTypes.ARRAY(DataTypes.INTEGER),
+      defaultValue: []
+    },
+    teams: {
+      type: DataTypes.ARRAY(DataTypes.INTEGER),
+      defaultValue: []
+    }
   },
-  first_name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  last_name: {
-    type: DataTypes.STRING,
-    allowNull: false
+  {
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
   }
-},{
-  tableName: 'member',
-  timestamps: false
-})
+)
 
 export { Member }
